52a368670c00d8d3895a8f214baee5da83b0d1c4
Chirag Saxena <saxenachirag053@gmail.com>
2026-03-15 22:10:48 +0530

    Day 4 - CodeChef Chef and Dolls (XOR solution)

---
+++ DAY4/ChefandDolls.java
@@ -0,0 +1,24 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class ChefandDolls {
+
+    public static void main(String[] args) throws java.lang.Exception {
+        // your code goes here
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            int xor = 0;
+
+            for (int i = 0; i < n; i++) {
+                xor ^= sc.nextInt();
+            }
+
+            System.out.println(xor);
+        }
+    }
+}
