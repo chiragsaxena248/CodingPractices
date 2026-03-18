d4f8aef384de5ce15657fde697b8095940a576d3
Chirag Saxena <saxenachirag053@gmail.com>
2026-03-18 21:49:41 +0530

    Day 7

---
+++ DAY7/EqualBuying.java
@@ -0,0 +1,19 @@
+
+import java.lang.*;
+import java.util.*;
+
+class EqualBuying {
+
+    public static void main(String[] args) throws java.lang.Exception {
+        // your code goes here
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        if (n % 3 == 0) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+
+    }
+}
