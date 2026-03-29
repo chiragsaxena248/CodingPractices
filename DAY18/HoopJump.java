53e13f68f8d7b6bdbaa995d802fc7cb944fb8c5b
Chirag Saxena <saxenachirag053@gmail.com>
2026-03-29 18:32:24 +0530

    Day 18 push

---
+++ DAY18/HoopJump.java
@@ -0,0 +1,19 @@
+
+import java.lang.*;
+import java.util.*;
+
+class HoopJump {
+
+    public static void main(String[] args) throws java.lang.Exception {
+        // your code goes here
+
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            int start = 0;
+            System.out.println((n + 1) / 2);
+        }
+
+    }
+}
