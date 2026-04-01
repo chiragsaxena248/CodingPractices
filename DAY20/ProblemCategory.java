fa740296138f096ed6b8b16abd2e9da0067df434
Chirag Saxena <saxenachirag053@gmail.com>
2026-04-01 18:27:53 +0530

    DAY 20 push

---
+++ DAY20/ProblemCategory.java
@@ -0,0 +1,22 @@
+
+import java.lang.*;
+import java.util.*;
+
+class ProblemCategory {
+
+    public static void main(String[] args) throws java.lang.Exception {
+        // your code goes here
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int x = sc.nextInt();
+            if (x >= 1 && x < 100) {
+                System.out.println("Easy");
+            } else if (x >= 100 && x < 200) {
+                System.out.println("Medium");
+            } else if (x >= 200 && x <= 300) {
+                System.out.println("Hard");
+            }
+        }
+    }
+}
